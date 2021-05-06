1d81405e07d8887444d89da237c4dc5d3ddbc08d
IhorB <igorbat15@hotmail.com>
2021-05-06 16:07:10 -0400

    Camera initial work, takes pic and returns thumbnail

--- app/src/main/java/com/example/photoorganizer/MainActivity.kt
+++ app/src/main/java/com/example/photoorganizer/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.photoorganizer
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import com.example.photoorganizer.databinding.ActivityMainBinding
+import com.example.photoorganizer.utils.FileUtil
+import java.io.File
+
+// TODO: Guide-> https://developer.android.com/training/camera/photobasics
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bundledMainActivity: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //bundledMainActivity = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        val btn: Button = findViewById(R.id.btnTest)
+        btn.setOnClickListener {
+            val fileUtil: FileUtil = FileUtil(this, applicationContext)
+            fileUtil.dispatchTakePictureIntent()
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            val image: ImageView = findViewById(R.id.ivTestImage)
+            image.setImageBitmap(imageBitmap)
+        }
     }
 }
---
+++ app/src/main/java/com/example/photoorganizer/utils/FileUtil.kt
@@ -0,0 +1,61 @@
+package com.example.photoorganizer.utils
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+open class FileUtil(private val activity: Activity, private val context: Context) {
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    lateinit var currentPhotoPath: String
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(context.packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        context,
+                        "com.example.photoorganizer",
+                        it
+                    )
+                    //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(activity, takePictureIntent, REQUEST_IMAGE_CAPTURE, null)
+                }
+            }
+        }
+    }
+}
