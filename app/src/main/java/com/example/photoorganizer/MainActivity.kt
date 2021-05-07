b51a2051cfc0851b64b1f3f1867161cd6ac8a5e4
IhorB <igorbat15@hotmail.com>
2021-05-07 14:55:21 -0400

    Some image work

--- app/src/main/java/com/example/photoorganizer/MainActivity.kt
+++ app/src/main/java/com/example/photoorganizer/MainActivity.kt
@@ -4,36 +4,62 @@ import android.content.Intent
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
 import com.example.photoorganizer.databinding.ActivityMainBinding
+import com.example.photoorganizer.utils.DEBUG_TAG
 import com.example.photoorganizer.utils.FileUtil
+import com.example.photoorganizer.utils.REQUEST_IMAGE_CAPTURE
+import timber.log.Timber
 import java.io.File
 
 // TODO: Guide-> https://developer.android.com/training/camera/photobasics
+// https://guides.codepath.com/android/Accessing-the-Camera-and-Stored-Media
 
 class MainActivity : AppCompatActivity() {
     lateinit var bundledMainActivity: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //bundledMainActivity = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        bundledMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bundledMainActivity.root)
+
+        Timber.plant(Timber.DebugTree())
+        Timber.tag(DEBUG_TAG).d("* * * Started App * * *")
+
         val btn: Button = findViewById(R.id.btnTest)
         btn.setOnClickListener {
             val fileUtil: FileUtil = FileUtil(this, applicationContext)
             fileUtil.dispatchTakePictureIntent()
         }
+
+
     }
 
+    override fun onStart() {
+        super.onStart()
 
+        val storageDir: File? = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val absPath = storageDir?.absolutePath
+        val isDir = storageDir?.isDirectory.toString()
+        Timber.tag(DEBUG_TAG).d("File $absPath is dir=$isDir ")
+    }
+
+    /**
+     * Returned image should be coming here if takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+     * is disabled in Util class
+     */
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            val image: ImageView = findViewById(R.id.ivTestImage)
-            image.setImageBitmap(imageBitmap)
+        Timber.tag(DEBUG_TAG).d("onActivityResult()")
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Timber.tag(DEBUG_TAG).d("image taken ...")
+            //val imageBitmap = data?.extras?.get("data") as Bitmap
+            //val image: ImageView = findViewById(R.id.ivTestImage)
+            //image.setImageBitmap(imageBitmap)
         }
     }
 }
--- app/src/main/java/com/example/photoorganizer/utils/FileUtil.kt
+++ app/src/main/java/com/example/photoorganizer/utils/FileUtil.kt
@@ -8,14 +8,16 @@ import android.os.Environment
 import android.provider.MediaStore
 import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.core.content.FileProvider
+import timber.log.Timber
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
-open class FileUtil(private val activity: Activity, private val context: Context) {
+val REQUEST_IMAGE_CAPTURE = 1
+val DEBUG_TAG = "DEBUGZ"
 
-    val REQUEST_IMAGE_CAPTURE = 1
+open class FileUtil(private val activity: Activity, private val context: Context) {
 
     lateinit var currentPhotoPath: String
 
@@ -25,12 +27,14 @@ open class FileUtil(private val activity: Activity, private val context: Context
         val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(
-            "JPEG_${timeStamp}_", /* prefix */
+            //"JPEG_${timeStamp}_", /* prefix */
+            "JPEG_TEST_", /* prefix */
             ".jpg", /* suffix */
             storageDir /* directory */
         ).apply {
             // Save a file: path for use with ACTION_VIEW intents
             currentPhotoPath = absolutePath
+            Timber.tag(DEBUG_TAG).d("photoPath -> $currentPhotoPath")
         }
     }
 
@@ -52,7 +56,8 @@ open class FileUtil(private val activity: Activity, private val context: Context
                         "com.example.photoorganizer",
                         it
                     )
-                    //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    Timber.tag(DEBUG_TAG).d("Starting Picture Intent")
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
                     startActivityForResult(activity, takePictureIntent, REQUEST_IMAGE_CAPTURE, null)
                 }
             }
