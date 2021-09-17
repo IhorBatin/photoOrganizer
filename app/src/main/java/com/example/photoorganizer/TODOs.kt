1eef9dd37a4ac3ed8d080faedb0ea4c15f80394f
IhorB <igorbat15@hotmail.com>
2021-09-17 12:07:56 -0400

    Fix to open image slider in correct position

---
+++ app/src/main/java/com/example/photoorganizer/TODOs.kt
@@ -0,0 +1,19 @@
+package com.example.photoorganizer
+
+// TODO`s
+
+/** Tasks **/
+// TODO: Add Share & Delete single image functionality from list or fullscreen view
+// TODO: Add ability to share & delete multiple images from list
+
+/** Fixes and Bugs */
+// TODO: Clean nup logic code in activity and view model before proceeding with ViewPage
+// TODO: Fix when screen rotated returns back to home directory
+// TODO: Fix folder names text color for dark mode
+// TODO: Fix delete/share menu overlapping bottom images, add some shown from bottom animation
+
+/** Future Plans/Features */
+// TODO: Add locking app and specific folder feature [use ext functions on File obj to to do this]
+// TODO: Add ability to change num of columns (1-2-3-4-5 on pinch or zoom with fingers/ add chooser on top)
+// TODO: When Navigated to folder update its name in toolbar instead of app name
+// TODO: apply default picture for folder. OR add ability to choose folder color.
